28ebb36cc30f42ba49889ce69de23829a85fd6aa
helpfulrobot <chris@silverstripe.com>
2016-01-01 06:12:27 +1300

    Converted to PSR-2

--- code/CustomMenu.php
+++ code/CustomMenu.php
@@ -1,19 +1,16 @@
 <?php
 
-class CustomMenu extends Extension {
-	
-	public function CustomMenu($slug = NULL) {
-		
-		if ($slug != NULL) {
-			
-			if ($Menu = Menu::get()->filter('Slug', $slug)->First()) {
-				return $Menu->MenuItems();	
-			} else {
-				return NULL;
-			}
-			
-		}
-		
-	}
-	
+class CustomMenu extends Extension
+{
+    
+    public function CustomMenu($slug = null)
+    {
+        if ($slug != null) {
+            if ($Menu = Menu::get()->filter('Slug', $slug)->First()) {
+                return $Menu->MenuItems();
+            } else {
+                return null;
+            }
+        }
+    }
 }
--- code/MenuItem.php
+++ code/MenuItem.php
@@ -1,38 +1,39 @@
 <?php
 
-class MenuItem extends DataObject {
-	
-	public static $db = array(
-		'MenuTitleOverride' => 'Varchar(50)',
-		'SortOrder' 		=> 'Int'
-	);
-	
-	public static $has_one = array(
-		'Menu' => 'Menu',
-		'Page' => 'Page'
-	);
-	
-	public static $default_sort = 'SortOrder';
-	
-	public function getMenuTitle() {
-		return $this->MenuTitleOverride ? $this->MenuTitleOverride : $this->Page()->MenuTitle;
-	}
-	
-	public function getLink() {
-		return $this->Page() ? $this->Page()->Link() : NULL;
-	}
-	
-	public function getCMSFields() {
-		
-		$fields = new FieldList( 
-			new TextField('MenuTitleOverride', 'Title override'),
-			new TreeDropdownField('PageID', 'Page', 'Page')
-		);
-		
-		$this->extend('updateCMSFields', $fields);
-		
-		return $fields;
-		
-	}
-	
+class MenuItem extends DataObject
+{
+    
+    public static $db = array(
+        'MenuTitleOverride' => 'Varchar(50)',
+        'SortOrder'        => 'Int'
+    );
+    
+    public static $has_one = array(
+        'Menu' => 'Menu',
+        'Page' => 'Page'
+    );
+    
+    public static $default_sort = 'SortOrder';
+    
+    public function getMenuTitle()
+    {
+        return $this->MenuTitleOverride ? $this->MenuTitleOverride : $this->Page()->MenuTitle;
+    }
+    
+    public function getLink()
+    {
+        return $this->Page() ? $this->Page()->Link() : null;
+    }
+    
+    public function getCMSFields()
+    {
+        $fields = new FieldList(
+            new TextField('MenuTitleOverride', 'Title override'),
+            new TreeDropdownField('PageID', 'Page', 'Page')
+        );
+        
+        $this->extend('updateCMSFields', $fields);
+        
+        return $fields;
+    }
 }
